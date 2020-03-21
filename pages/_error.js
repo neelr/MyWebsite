e7f1ca873fef1243522dcef2f2000e9d8a260959
Neel Redkar <neel.redkar@outlook.com>
2020-03-20 18:11:02 -0700

    kai forced me

--- pages/_error.js
+++ pages/_error.js
@@ -3,11 +3,11 @@ import Layout from "../components/Layout";
 import { FaGithub, FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
 
 export default class Index extends React.Component {
-    render () {
-        return(
+    render() {
+        return (
             <Layout>
-                <h1 style={{color:"#e8233e"}}>Error 404!</h1>
-                <h2>Seem's that page wasn't found....</h2>
+                <h1 style={{ color: "#e8233e" }}>Error 404!</h1>
+                <h2>Seems that page wasn't found....</h2>
             </Layout>
         )
     }
