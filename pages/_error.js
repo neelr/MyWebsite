d0ac7560a75346b473b19c3222c7f5ffef971076
Neel Redkar <neel.redkar@gmail.com>
2021-02-06 14:37:20 -0800

    switch to plug and play

--- pages/_error.js
+++ pages/_error.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { FaGithub, FaFacebookSquare, FaLinkedin } from 'react-icons/fa';
 
 export default class Index extends React.Component {
     render() {
