683d406d25003e687c86a1d566099a5838baa24d
Neel Redkar <neel.redkar@gmail.com>
2022-11-23 13:18:12 -0800

    add ico file instead

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>@neelr</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/hippo.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
