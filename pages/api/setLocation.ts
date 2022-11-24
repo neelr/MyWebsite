414996341787d056c8610bb5493221fd9b276c35
Neel Redkar <neel.redkar@gmail.com>
2022-11-24 04:53:25 -0800

    add request checking

--- pages/api/setLocation.ts
+++ pages/api/setLocation.ts
@@ -6,19 +6,26 @@ const supabaseKey = `${process.env.DATABASE_KEY}`;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export type Data = {
-  error: PostgrestError | null;
+  error: PostgrestError | null | string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { data, error } = await supabase
-    .from("locations")
-    .upsert({
+  // check auth
+  if (req.headers.authorization !== `Bearer ${process.env.LOCATION_KEY}`)
+    return res.status(401).json({ error: "Not Authed" });
+  // check if req.body is valid
+
+  if (req.body.city && req.body.weather) {
+    await supabase.from("locations").upsert({
       id: Date.now(),
       city: req.body.location,
       weather: req.body.weather,
     });
-  res.status(200).json({ error });
+  } else {
+    return res.status(400).json({ error: "Bad Request" });
+  }
+  res.status(200).json({ error: null });
 }
