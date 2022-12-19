72b6bcef45477536d8e09f41f20becc57b5078a4
Neel Redkar <neel.redkar@gmail.com>
2022-12-18 17:10:50 -0800

    add thumbnail & meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>@neelr</title>
+        <meta name="title" content="@neelr" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="find out a little about me!" />
+        <meta name="og:title" content="@neelr" />
+        <meta name="og:description" content="find out a little about me!" />
+        <meta name="og:image" content="https://neelr.vercel.app/thumbnail.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
